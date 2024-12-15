63caf9dfc50365320f62d601aedc1e3eac0021f6
rk <ranjitkale0001@gmail.com>
2024-12-15 10:03:39 +0530

    Basic program
    Intial javaScript

---
+++ Demo.js
@@ -0,0 +1,9 @@
+console.log("Weelcome to JavaScript!")
+
+//Using powerShell:-
+// PS C:\Users\user\Documents\GitHub\JavaScript> node .\Demo.js   
+// Weelcome to JavaScript!
+
+// Using cmd
+// C:\Users\user\Documents\GitHub\JavaScript>node Demo.js
+// Weelcome to JavaScript!
