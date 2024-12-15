8715b11a646c3b8b52f3d285aaf2bb34526ba179
rk <ranjitkale0001@gmail.com>
2024-12-15 10:54:06 +0530

    comments & GobalVariables

---
+++ Core JavaScript/Comment.js
@@ -0,0 +1,4 @@
+/* It is multi line comment.  
+It will not be displayed */ 
+
+// It is single line comment  
---
+++ Core JavaScript/GobalVariable.js
@@ -0,0 +1,14 @@
+var value=50;//global variable  
+function a(){  
+console.log("a:-",value);
+}  
+function b(){   
+console.log("b:-",value);
+}
+a();
+b();
+
+/*
+a:- 50
+b:- 50
+*/
